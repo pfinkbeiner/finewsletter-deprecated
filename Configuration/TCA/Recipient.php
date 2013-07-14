83f61539549dc4c9e1296909307996862f8be2b6
Patrick Finkbeiner <finkbeiner.patrick@gmail.com>
2013-07-14 17:38:33 +0200

    [BUGFIX] TCA camelCase format.

--- Configuration/TCA/Recipient.php
+++ Configuration/TCA/Recipient.php
@@ -6,10 +6,10 @@ if (!defined ('TYPO3_MODE')) {
 $TCA['tx_finewsletter_domain_model_recipient'] = array(
 	'ctrl' => $TCA['tx_finewsletter_domain_model_recipient']['ctrl'],
 	'interface' => array(
-		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, email, name, firstName, lastName, language, active, token',
+		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, email, name, first_name, last_name, language, active, token',
 	),
 	'types' => array(
-		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, email, name, firstName, lastName, language, active, token,--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,starttime, endtime'),
+		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, email, name, first_name, last_name, language, active, token,--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,starttime, endtime'),
 	),
 	'palettes' => array(
 		'1' => array('showitem' => ''),
@@ -111,7 +111,7 @@ $TCA['tx_finewsletter_domain_model_recipient'] = array(
 				'eval' => 'trim,required'
 			),
 		),
-		'firstName' => array(
+		'first_name' => array(
 			'exclude' => 0,
 			'label' => 'LLL:EXT:finewsletter/Resources/Private/Language/locallang_db.xml:tx_finewsletter_domain_model_recipient.firstName',
 			'config' => array(
@@ -120,7 +120,7 @@ $TCA['tx_finewsletter_domain_model_recipient'] = array(
 				'eval' => 'trim,required'
 			),
 		),
-		'lastName' => array(
+		'last_name' => array(
 			'exclude' => 0,
 			'label' => 'LLL:EXT:finewsletter/Resources/Private/Language/locallang_db.xml:tx_finewsletter_domain_model_recipient.lastName',
 			'config' => array(
